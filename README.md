9f986b90ce9ca2fa69826e9237a19bbcfee89a1e
rlarnfktkd1 <ksj8367@gmail.,com>
2019-05-13 21:30:51 +0900

    API 50%

--- README.md
+++ README.md
@@ -10,10 +10,10 @@ Prisma 설명
 
 - [x] Create account
 - [x] Request Secret
-- [ ] Confirm Secret (Login)
-- [ ] Log in - Page 처음 접속시 이메일 입력, 사용자의 이메일로 비밀값 전송, nodemailer이용
-- [ ] Like / Unlike a photo
-- [ ] Comment on a photo
+- [x] Confirm Secret (Login)
+- [x] Log in - Page 처음 접속시 이메일 입력, 사용자의 이메일로 비밀값 전송, nodemailer이용
+- [x] Like / Unlike a photo
+- [x] Comment on a photo
 - [ ] Search by user
 - [ ] Search by location
 - [ ] See user profile
--- src/api/User/requsetSecret/requestSecret.js
+++ src/api/User/requsetSecret/requestSecret.js
@@ -1,15 +1,14 @@
-import { generateSecret } from "../../../utils";
+import { generateSecret, sendSecretMail } from "../../../utils";
 import { prisma } from "../../../../generated/prisma-client";
-import { sendSecretMail } from "../../../utils";
 
 export default {
   Mutation: {
     requestSecret: async (_, args, { request }) => {
+      console.log(request.user);
       const { email } = args;
       const loginSecret = generateSecret();
       console.log(loginSecret);
       try {
-        throw Error();
         await sendSecretMail(email, loginSecret);
         await prisma.updateUser({ data: { loginSecret }, where: { email } });
         return true;
