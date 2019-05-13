0ef9240fbf5596c39b70b4d1805a34551ecc686c
rlarnfktkd1 <ksj8367@gmail.,com>
2019-05-13 13:29:49 +0900

    Setting API with Email

--- README.md
+++ README.md
@@ -5,3 +5,20 @@ Instagram clone with Express + Prisma + React + Native
 Prisma 설명
 
 ![Prisma](./images/image1.PNG)
+
+## User Stories
+
+- [x] Create account
+- [ ] Request Secret
+- [ ] Confirm Secret (Login)
+- [ ] Log in - Page 처음 접속시 이메일 입력, 사용자의 이메일로 비밀값 전송, nodemailer이용
+- [ ] Like / Unlike a photo
+- [ ] Comment on a photo
+- [ ] Search by user
+- [ ] Search by location
+- [ ] See user profile
+- [ ] Follow / Unfolliw User
+- [ ] See the Full photo
+- [ ] Upload a Photos
+- [ ] Edit the Photo
+- [ ] See the feed
---
+++ src/utils.js
@@ -0,0 +1,33 @@
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config({ path: path.resolve(__dirname, ".env") });
+
+import { adjectives, nouns } from "./words";
+import nodemailer from "nodemailer";
+import sgTransport from "nodemailer-sendgrid-transport";
+
+export const generateSecret = () => {
+  const randomNumber = Math.floor(Math.random() * adjectives.length);
+  return `${adjectives[randomNumber]} ${nouns[randomNumber]}`;
+};
+
+export const sendMail = email => {
+  const options = {
+    auth: {
+      api_user: process.env.SENDGRID_USERNAME,
+      api_key: process.env.SENDGRID_PASSWORD
+    }
+  };
+  const client = nodemailer.createTransport(sgTransport(options));
+  return client.sendMail(email);
+};
+
+export const sendSecretMail = (address, secret) => {
+  const email = {
+    from: "sangheongram@sangheongram.com",
+    to: address,
+    subject: "Login Secret for Sangheongram🔒",
+    html: `Hello Your login secret it ${secret}.<br/> 로그인을 위해 앱/웹 에 복사 후 붙여넣어주세요`
+  };
+  return sendMail(email);
+};
